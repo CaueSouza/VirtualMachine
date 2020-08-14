efdc3f021aff409b8ac33ef9d4e0ef04ee72d3e8
CaueSouza <caue.souzabarros@hotmail.com>
2020-08-14 10:19:11 -0300

    Adicionar arquivos de projeto.

---
+++ MainForm.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Security;
+using System.Text;
+using System.Windows.Forms;
+
+namespace VirtualMachine
+{
+    public partial class MainForm : Form
+    {
+        private OpenFileDialog openFileDialog;
+        private Stack codeStack;
+
+        public MainForm()
+        {
+            openFileDialog = new OpenFileDialog()
+            {
+                FileName = "Select a text file",
+                Filter = "Text files (*.txt)|*.txt",
+                Title = "Open text file"
+            };
+
+            InitializeComponent();
+        }
+
+        private void parseFileCommands(String filePath)
+        {
+            string[] commands = System.IO.File.ReadAllLines(filePath);
+
+            foreach (string command in commands)
+            {
+                codeStack.push(command);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    var filePath = openFileDialog.FileName;
+                    using (Stream str = openFileDialog.OpenFile())
+                    {
+                        parseFileCommands(filePath);
+                    }
+                }
+                catch (SecurityException ex)
+                {
+                    MessageBox.Show($"Security error.\n\nError message: {ex.Message}\n\n" +
+                    $"Details:\n\n{ex.StackTrace}");
+                }
+            }
+        }
+    }
+}
---
+++ Stack.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VirtualMachine
+{
+    class Stack
+    {
+		private int top;
+		private int capacity;
+		private String[] array;
+
+		public Stack()
+		{
+			capacity = 1;
+			array = new String[capacity];
+			top = -1;
+		}
+
+		public void push(String data)
+		{
+			if (isFull())
+			{
+				expandArray(); // if array is full then increase its capacity
+			}
+			array[++top] = data; // insert the data
+		}
+
+		public String pop()
+		{
+			if (isEmpty())
+			{
+				Console.WriteLine("Stack is empty");
+				return null;
+			}
+			else
+			{
+				reduceSize(); // function to check if size can be reduced
+				return array[top--];
+			}
+		}
+
+		public bool isFull()
+		{
+			if (capacity == top + 1)
+				return true;
+			else
+				return false;
+		}
+
+		public bool isEmpty()
+		{
+			if (top == -1)
+				return true;
+			else
+				return false;
+		}
+
+		public void expandArray()
+		{
+			int curr_size = top + 1;
+			String[] new_array = new String[curr_size * 2];
+			for (int i = 0; i < curr_size; i++)
+			{
+				new_array[i] = array[i];
+			}
+			array = new_array; // refer to the new array
+			capacity = new_array.Length;
+		}
+
+		public void reduceSize()
+		{
+			int curr_length = top + 1;
+			if (curr_length < capacity / 2)
+			{
+				String[] new_array = new String[capacity / 2];
+				Array.Copy(array, 0, new_array, 0, new_array.Length);
+				array = new_array;
+				capacity = new_array.Length;
+			}
+		}
+
+		public int getLength()
+		{
+			return top;
+		}
+
+		public String getPosition(int position)
+		{
+			if (position > top)
+				return "";
+			else
+			{
+				return array[position];
+			}
+		}
+
+		public void display()
+		{
+			for (int i = 0; i <= top; i++)
+			{
+				Console.WriteLine(array[i] + "=>");
+			}
+			Console.WriteLine();
+			Console.WriteLine("ARRAY SIZE:" + array.Length);
+		}
+
+		public void cleanStack()
+		{
+			top = -1;
+		}
+	}
+}
